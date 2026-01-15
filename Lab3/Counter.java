8aab46f89eafbbd420fc02675bba3137ffaf7860
Arnav Kothekar <98852337+ArnavKothekar@users.noreply.github.com>
2026-01-15 13:03:55 -0500

    Add Lab 3 Files

---
+++ Lab3/Counter.java
@@ -0,0 +1,53 @@
+package coe318.lab3;
+
+public class Counter {
+
+    private int modulus, digit;
+    Counter left;
+
+    public Counter(int modulus, Counter left) {
+        this.modulus = modulus;
+        this.digit = 0;
+        this.left = left;
+    }
+
+    public int getModulus() {
+        return this.modulus;
+    }
+
+    public Counter getLeft() {
+        return this.left;
+    }
+
+    public int getDigit() {
+        return this.digit;
+    }
+
+    public void setDigit(int digit) {
+        this.digit = digit;
+    }
+
+    public void increment() {
+        this.digit++;
+        if (this.digit >= this.modulus){
+            this.digit = 0;
+            if (this.left != null){
+                this.left.increment();
+            }
+        }
+    }
+
+    public int getCount() {
+        if (this.left != null){
+            return this.digit + this.modulus * this.left.getCount();
+        } else {
+            return this.digit;
+        }
+    }
+
+    @Override
+    public String toString() {
+        //DO NOT MODIFY THIS CODE
+        return "" + getCount();
+    }
+}
